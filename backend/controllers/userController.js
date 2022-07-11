dd08935d2e884782c1d8ff78462e0eb01254fc3b
Vladyslav <makarellajs@gmail.com>
2022-07-11 22:37:41 +0300

    Initial signin and signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,12 +30,19 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Invalid user data');
   }
-
-  res.json({ message: 'register user' });
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'login user' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({ _id: user.id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
 });
 
 const getMe = asyncHandler(async (req, res) => {
