71c39a0c810693a5598697506f4f42c5330cee62
Vladyslav <makarellajs@gmail.com>
2022-07-14 21:27:45 +0300

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { errorHandler } from './middleware/errorMiddleware.js';
 import goalRouter from './routes/goalRoutes.js';
@@ -17,6 +19,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', goalRouter);
 app.use('/api/users', userRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    );
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Set to production!');
+  });
+}
+
 app.use(errorHandler);
 
 app.listen(
